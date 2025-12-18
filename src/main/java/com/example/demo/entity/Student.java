55e3341302c0f0ee343a4f55465399252a5597c0
bala-amypo <bala@amypo.in>
2025-12-18 14:29:47 +0000

    User commit

--- src/main/java/com/example/demo/entity/Student.java
+++ src/main/java/com/example/demo/entity/Student.java
@@ -1,10 +1,28 @@
 package com.example.demo.entity;
 //import Student from entity;
 
+@Entity
 public class Student{
-    String name;
-    Long id;
-    int password;
-    String role;
+
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private int id;
+    private String name;
+    private int password;
+    private String role;
 }
-public class Student()
+public Student(int id, String name, String password, String role){
+    this.id = id;
+    this.name = name;
+    this.password = password;
+    this.role = role;
+}
+public Student(){
+
+}
+public int getId(){
+    return id;
+}
+public void setId(int id){
+    
+}
