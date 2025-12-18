eb23a7c3a832e4dc2398c441e054c27192b23b8a
bala-amypo <bala@amypo.in>
2025-12-18 15:23:59 +0000

    User commit

--- src/main/java/com/example/demo/entity/Influencer.java
+++ src/main/java/com/example/demo/entity/Influencer.java
@@ -4,4 +4,13 @@ package com.example.demo.entity;
 public class Influencer{
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    private Long id;
+    private String name;
+    private String socialHandle;
+    private boolean active;
 } 
+public Influencer{
+
+}
+public Influencer(String name, String socialHandle, )
