53135fa0bb0b75732f311381a53a921d543403f3
bala-amypo <bala@amypo.in>
2025-12-19 04:24:07 +0000

    User commit

--- src/main/java/com/example/demo/entity/Influencer.java
+++ src/main/java/com/example/demo/entity/Influencer.java
@@ -1,23 +1,72 @@
 package com.example.demo.entity;
+
+
 import jakarta.persistence.*;
+import java.util.List;
 
+@Entity
+@Table(name = "influencers")
+public class Influencer {
 
-public class Influencer{
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-
     private Long id;
+
     private String name;
+
+    @Column(unique = true)
     private String socialHandle;
-    private boolean active;
 
-public Influencer{
+    private boolean active = true;
 
-}
-public Influencer(String name, String socialHandle, boolean active){
-    this.name = name;
-    this.socialHandle = socialHandle;
-    this.active = active;
-}
+    @OneToMany(mappedBy = "influencer")
+    private List<DiscountCode> discountCodes;
 
+    public Influencer() {}
+
+    public Influencer(String name, String socialHandle, boolean active) {
+        this.name = name;
+        this.socialHandle = socialHandle;
+        this.active = active;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSocialHandle() {
+        return socialHandle;
+    }
+
+    public void setSocialHandle(String socialHandle) {
+        this.socialHandle = socialHandle;
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
+    }
+
+    public List<DiscountCode> getDiscountCodes() {
+        return discountCodes;
+    }
+
+    public void setDiscountCodes(List<DiscountCode> discountCodes) {
+        this.discountCodes = discountCodes;
+    }
 }
