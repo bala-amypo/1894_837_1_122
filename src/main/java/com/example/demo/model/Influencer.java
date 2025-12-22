17e43a05d43a2333c03e388ca77c2decb7d4aacc
bala-amypo <bala@amypo.in>
2025-12-22 08:30:55 +0000

    User commit

--- src/main/java/com/example/demo/model/Influencer.java
+++ src/main/java/com/example/demo/model/Influencer.java
@@ -1,16 +1,34 @@
 package com.example.demo.model;
 
-import jakarta.persistence.*;
-import lombok.Data;
-
-@Data
-@Entity
 public class Influencer {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
     private String name;
-    private String socialHandle;
+    private String email;
+
+    public Influencer() {}
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
