05f7dadbfb4ca73f1aeaaee2112f150e7d0b9e39
bala-amypo <bala@amypo.in>
2025-12-22 08:27:52 +0000

    User commit

--- src/main/java/com/example/demo/ServiceImplementation/InfluencerServiceImpl.java
+++ src/main/java/com/example/demo/ServiceImplementation/InfluencerServiceImpl.java
@@ -1,8 +1,27 @@
-import com.example.demo.service.impl;
+package com.example.demo.service.impl;
+
+import com.example.demo.model.Influencer;
+import com.example.demo.service.InfluencerService;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class InfluencerServiceImpl implements InfluencerService {
-    public Influencer createInfluencer(Influencer i){ return i; }
-    public List<Influencer> getAllInfluencers(){ return new ArrayList<>(); }
-    public Influencer getInfluencerById(Long id){ return new Influencer(); }
+
+    @Override
+    public Influencer createInfluencer(Influencer influencer) {
+        return influencer;
+    }
+
+    @Override
+    public List<Influencer> getAllInfluencers() {
+        return new ArrayList<>();
+    }
+
+    @Override
+    public Influencer getInfluencerById(Long id) {
+        return new Influencer();
+    }
 }
