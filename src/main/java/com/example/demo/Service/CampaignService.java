870282cabb1203ea8b1f615d03c21a3c098e20b8
bala-amypo <bala@amypo.in>
2025-12-22 09:15:53 +0000

    User commit

--- src/main/java/com/example/demo/Service/CampaignService.java
+++ src/main/java/com/example/demo/Service/CampaignService.java
@@ -1,8 +1,13 @@
 package com.example.demo.Service;
 
-import com.example.demo.model.Influencer;
+import com.example.demo.model.Campaign;
 import java.util.List;
 
 public interface CampaignService {
-    
+
+    Campaign createCampaign(Campaign campaign);
+
+    Campaign getCampaignById(Long id);
+
+    List<Campaign> getAllCampaigns();
 }
--- src/main/java/com/example/demo/Service/DiscountCodeService.java
+++ src/main/java/com/example/demo/Service/DiscountCodeService.java
@@ -1,8 +1,11 @@
 package com.example.demo.Service;
 
-import com.example.demo.model.Influencer;
+import com.example.demo.model.DiscountCode;
 import java.util.List;
 
 public interface DiscountCodeService {
-   
+
+    DiscountCode createDiscountCode(DiscountCode discountCode);
+
+    List<DiscountCode> getAllDiscountCodes();
 }
